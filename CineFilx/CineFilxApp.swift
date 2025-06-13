eb861bf5d9efa8bcb80ac07fabc0201b6dd73be0
Adithyan N A <itIos04@manappuram.com>
2025-06-13 20:57:43 +0530

    Initial Commit

---
+++ CineFilx/CineFilxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineFilxApp.swift
+//  CineFilx
+//
+//  Created by adithyan na on 13/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CineFilxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineFilxTests/CineFilxTests.swift
@@ -0,0 +1,17 @@
+//
+//  CineFilxTests.swift
+//  CineFilxTests
+//
+//  Created by adithyan na on 13/6/25.
+//
+
+import Testing
+@testable import CineFilx
+
+struct CineFilxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
